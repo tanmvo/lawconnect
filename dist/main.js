3b9e8b8d186bd5b13b7c4b5304b79b20d59883a2
Tan <tan.m.vo@gmail.com>
2018-11-25 17:56:13 +1100

    Logo and Sidebar containers styled

--- dist/main.js
+++ dist/main.js
@@ -7,17 +7,20 @@ var app = {
 	// Sidebar
 	menuIcon: $('.menu-icon'),
 	appContainer: $('.app'),
+	body: $('body'),
 	toggleSidebar: function() {
 		app.menuIcon.on('click', function() {
 			app.appContainer.toggleClass('app--sidebar-open');
+			app.body.toggleClass('overflow-none');
 		});
 	},
 	resetSidebar: function() {
 		// If user resizes the window > 575px and sidebar is open,
 		// Remove class to readjust components into its correct position  
 		$(window).resize(function() {
-			if ($(window).width() > 575) {
+			if ($(window).width() > 575 && app.appContainer.hasClass('app--sidebar-open')) {
 				app.appContainer.removeClass('app--sidebar-open');
+				app.body.removeClass('overflow-none');
 			}
 		});
 	}
